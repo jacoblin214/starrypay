102a348603f1717592242648d22f9d01efafe2c1
jacoblin214 <993043389@qq.com>
2018-01-19 11:43:23 +0800

    完成文章详情细节、产品中心 banner 跟着tab联动

--- src/js/product-center.js
+++ src/js/product-center.js
@@ -2,15 +2,16 @@
  * @Author: jecyu
  * @Date: 2018-01-18 9:24:40 pm
  * @Modified By: jeCyu
- * @Last Modified time: 2018-01-18 9:28:32 pm
+ * @Last Modified time: 2018-01-19 10:56:07 am
  */
 
 /**
  * 更换 tab 内容
  * @param {*} tabNav
  * @param {*} tabConent
+ * @param {string} bannerUrl 图片地址
  */
-function changeTab(tabNav, tabConentID) {
+function changeTab(tabNav, tabConentID, bannerUrl) {
     // 遍历 a 链接，添加 active
     tabNav.each(function(index) {
         if ($(this).attr("data-targetTab") === tabConentID) {
@@ -27,6 +28,13 @@ function changeTab(tabNav, tabConentID) {
         .show()
         .siblings()
         .hide();
+
+    // 显示对应的 banner
+    // 找到 banner
+    var $banner = $(".section-banner .banner");
+    var $banner_img = $banner.find("img");
+    // 图片地址
+    $banner_img.attr("src", bannerUrl);
 }
 
 // 路由
@@ -42,6 +50,9 @@ var tab_1 = "starrypay";
 var tab_2 = "certification";
 var tab_3 = "core_card";
 
+var tab_1_bannerUrl = "../images/product/product_banner-1.png";
+var tab_2_bannerUrl = "../images/product/product_banner-2.png";
+var tab_3_bannerUrl = "../images/product/product_banner-1.png";
 // 回调函数处理
 // for (let i = 1; i <= 7; i++) {
 //     // tab 变了
@@ -53,19 +64,22 @@ var tab_3 = "core_card";
 // }
 
 Router.route("/" + tab_1, function() {
-    changeTab($tabs_item_link, tab_1);
+    changeTab($tabs_item_link, tab_1, tab_1_bannerUrl);
 });
 
 Router.route("/" + tab_2, function() {
-    changeTab($tabs_item_link, tab_2);
+    changeTab($tabs_item_link, tab_2, tab_2_bannerUrl);
 });
 
 Router.route("/" + tab_3, function() {
-    changeTab($tabs_item_link, tab_3);
+    changeTab($tabs_item_link, tab_3, tab_3_bannerUrl);
 });
 
 $tabs_item_link.on("click", function() {
+    // 获取目标 tab
     let attr = $(this).attr("data-targetTab");
-    changeTab($tabs_item_link, attr);
+    // 获取 bannerUrl
+    let banner_url = $(this).attr("data-bannerUrl");
+    changeTab($tabs_item_link, attr, banner_url);
     return false; // 禁止链接的默认行为
 });
